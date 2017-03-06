c2e036407700f61ea54f6b9ccf4e010dc6b24f76
Homer1011110 <15629039251@163.com>
2017-03-06 11:31:39 +0800

    fix webpack config bug

--- src/background.js
+++
@@ -1,1 +0,0 @@
-console.log("hello world, this is background.js,", "version 1.0.0")
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,12 @@
+// webpack will wrap these code as a CMD module
+// so, dont't use es6's import&export features here
 const path = require("path")
 const copyWebpackPlugin = require("copy-webpack-plugin")
 
 const soureDir = "./src"
 
 const config = {
+  watch: true,
   entry: {
     background: soureDir + "/background_scripts/index.js",
     content: soureDir + "/content_scripts/index.js",
@@ -15,7 +18,11 @@ const config = {
   },
   plugins: [
     new copyWebpackPlugin([
-      {from: "./src/manifest.json", to: "manifest.json"}
+      {from: "./src/manifest.json", to: "manifest.json"},
+      {from: "./src/imgs", to: "imgs"},
+      {from: "./src/popup/index.html", to: "popup.html"},
+      {from: "./src/popup/index.css", to: "popup.css"},
+      {from: "./src/content_scripts/index.css", to: "content.css"}
     ], {
       copyUnmodified: true
     })
